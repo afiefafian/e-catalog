ef5990c292cf16a87b74ff966c849c7027d6ffca
Afief Afian <afiefafian95@gmail.com>
2019-02-19 23:49:44 +0700

    install tabler-ui bs4 admin template

---
+++ resources/views/layouts/dashboard/partial/_nav-head.blade.php
@@ -0,0 +1,38 @@
+<!-- nav-header -->
+<div class="header py-4">
+  <div class="container">
+    <div class="d-flex">
+      <a class="header-brand" href="{{ url('home') }}">
+        E-Catalog
+        {{-- <img src="./template/tabler-ui/demo/brand/tabler.svg" class="header-brand-img" alt="tabler logo"> --}}
+      </a>
+      <div class="d-flex order-lg-2 ml-auto">
+        <div class="dropdown">
+          <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
+            <span class="avatar" style="background-image: url(./demo/faces/female/25.jpg)"></span>
+            <span class="ml-2 d-none d-lg-block">
+              <span class="text-default">{{ Auth::user()->name }}</span>
+            </span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+            <a class="dropdown-item" href=""{{ route('user') }}">
+              <i class="dropdown-icon fe fe-user"></i> Profile
+            </a>
+            <a class="dropdown-item" href="{{ route('logout') }}" 
+              onclick="event.preventDefault(); 
+                document.getElementById('logout-form').submit();">
+              <i class="dropdown-icon fe fe-log-out"></i> Sign out
+            </a>
+          </div>
+        </div>
+      </div>
+      <a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#headerMenuCollapse">
+        <span class="header-toggler-icon"></span>
+      </a>
+    </div>
+  </div>
+</div>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+  @csrf
+</form>
---
+++ resources/views/layouts/dashboard/partial/_nav-menu.blade.php
@@ -0,0 +1,19 @@
+<div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg order-lg-first">
+        <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+          <li class="nav-item">
+            <a href="{{ url('home') }}" class="nav-link"><i class="fe fe-home"></i> Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="{{ url('supplier') }}" class="nav-link"><i class="fe fe-box"></i> Supplier</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="{{ url('produk') }}" class="nav-link"><i class="fe fe-check-square"></i> Produk</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
