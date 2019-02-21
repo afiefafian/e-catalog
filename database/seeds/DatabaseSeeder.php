0eecd72696c00f1fef973e78a965da1deac89d8c
Afief Afian <afiefafian95@gmail.com>
2019-02-22 04:15:45 +0700

    add seeding data, css/js production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
             IndoRegionProvinceSeeder::class,
             IndoRegionRegencySeeder::class,
             IndoRegionVillageSeeder::class,
+            SuppliersSeeder::class,
+            ProduksSeeder::class,
         ]);
     }
 }
